3b43afb8994a24ffbb71be6cffc2707c1ad426af
Chris Lo <christopher.lo@lexisnexisrisk.com>
2018-04-13 16:15:49 -0400

    updated and commit after checkout of master and submodules

---
+++ esp/src/Visualization/src/other/Legend.js
@@ -0,0 +1,152 @@
+"use strict";
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["d3", "./Table", "css!./Legend"], factory);
+    } else {
+        root.other_Legend = factory(root.d3, root.other_Table);
+    }
+}(this, function (d3, Table) {
+    function Legend() {
+        Table.call(this);
+        this._tag = "div";
+        
+        this.showHeader(false);
+    }
+    Legend.prototype = Object.create(Table.prototype);
+    Legend.prototype.constructor = Legend;
+    Legend.prototype._class += " other_Legend";
+
+    Legend.prototype.publish("dataFamily", "ND", "set", "Type of data",["1D","2D","ND", "any"],{tags:["Private"]});
+    Legend.prototype.publish("orientation", "vertical", "set", "Orientation of Legend rows",["vertical","horizontal"],{tags:["Private"]});
+    
+    Legend.prototype.targetWidget = function (widget) {
+        var context = this;
+        if(widget === undefined){
+            return context._targetWidget;
+        }
+            var colArr = ["Key", "Label"];
+            var dataArr = [];
+            var widgetColumns = widget.columns();
+
+            var paletteType = widget._palette.toString().split("function ")[1].split("(")[0];
+            if(paletteType === "ordinal"){
+                switch(this.dataFamily()){
+                    case '2D':
+                        dataArr = widget.data().map(function(n){
+                            return [_htmlColorBlock(widget._palette(n[0])),n[0]];
+                        });
+                        break;
+                    case 'ND':
+                        dataArr = widgetColumns.filter(function(n,i){return i>0;}).map(function(n){
+                            return [_htmlColorBlock(widget._palette(n)),n];
+                        });
+                        break;
+                }
+            } 
+            else if (paletteType === "rainbow"){
+                var colorArr = widget._palette.colors().reverse();
+                var steps = colorArr.length;
+                var weightMin = widget._dataMinWeight;
+                var weightMax = widget._dataMaxWeight;
+                for(var x = 0;x<steps;x++){
+                    var stepWeightDiff = parseInt((weightMax - weightMin) / steps);
+                    var lower,higher;
+                    if(x === 0){
+                        higher = commaSeparateNumber(weightMin + stepWeightDiff*(x+1));
+                        dataArr.push([_htmlColorBlock(colorArr[x]),"0 - " + higher]);
+                    } else if (x+1 === steps){
+                        lower = commaSeparateNumber(weightMin + (stepWeightDiff*x) + 1);
+                        dataArr.push([_htmlColorBlock(colorArr[x]),lower + "+"]);
+                    } else {
+                        lower = commaSeparateNumber(weightMin + (stepWeightDiff*x) + 1);
+                        higher = commaSeparateNumber(weightMin + stepWeightDiff*(x+1));
+                        dataArr.push([_htmlColorBlock(colorArr[x]),lower + " - " + higher]);
+                    }
+                }
+            }
+
+            context.columns(colArr);
+            context.data(dataArr);
+
+            context._targetWidget = widget;
+        
+        return this;
+        
+        function _htmlColorBlock(hexColor){
+            return "<div class=\"colorBlock\" style=\"background-color:"+hexColor+";\"></div>";
+        }
+        function commaSeparateNumber(val){
+            var int = val.toString().split(".")[0];
+            var dec = val.toString().split(".")[1];
+            while (/(\d+)(\d{3})/.test(int.toString())){
+                int = int.toString().replace(/(\d+)(\d{3})/, "$1"+","+"$2");
+            }
+            return typeof(dec) !== "undefined" ? int+"."+dec : int;
+        }
+    };
+
+    Legend.prototype.enter = function (domNode, element) {
+        Table.prototype.enter.apply(this, arguments);
+        this.renderHtmlDataCells(true);
+        this.fixedHeader(false);
+        this.fixedSize(true);
+        element.classed("other_Legend", true);
+    };
+
+    Legend.prototype.update = function (domNode, element) {
+        Table.prototype.update.apply(this, arguments);
+        
+        var context = this;
+        if(typeof (this._targetWidget) !== "undefined"){
+            this.targetWidget(this._targetWidget);
+        }
+        
+        element.classed("horiz-legend",this.orientation() === "horizontal");
+        
+        var table = element.select(".tableDiv > table");
+        var tableRect = table.node().getBoundingClientRect();
+        var elementRect = this._parentElement.node().getBoundingClientRect();
+        
+        element.select(".tableDiv").style({overflow:"visible"});
+        
+        var top = elementRect.height/2 - tableRect.height/2;
+        var left = elementRect.width/2 - tableRect.width/2;
+        table.style({position:"absolute",top:top+"px",left:left+"px"});
+        
+        var startIndex = this.pageNumber()-1;
+        var itemsOnPage = this.itemsPerPage();
+
+        var start = startIndex * itemsOnPage;
+        var end = parseInt(startIndex * itemsOnPage) + parseInt(itemsOnPage);
+
+        var tData = null;
+        if (this.pagination()) {
+            tData = this.data().slice(start,end);
+        } else {
+            tData = this.data();
+        }
+
+        var rows = this.tbody.selectAll("tr").data(tData);
+        rows
+            .on("click",function(d,i){
+                context.onClick(d,i);
+            })
+            .on("mouseover",function(d,i){
+                context.onMouseOver(d,i);
+            })
+        ;
+    };
+    
+    Legend.prototype.onClick = function (rowData,rowIdx) { 
+        console.log("Legend onClick method"); 
+        console.log("rowData: "+rowData);
+        console.log("rowIdx: "+rowIdx);
+    };
+    Legend.prototype.onMouseOver = function (rowData,rowIdx) {
+        console.log("Legend onMouseOver method"); 
+        console.log("rowData: "+rowData);
+        console.log("rowIdx: "+rowIdx);
+    };
+
+    return Legend;
+}));
---
+++ esp/src/Visualization/test/otherFactory.js
@@ -0,0 +1,243 @@
+﻿"use strict";
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else {
+        root.test_otherFactory = factory();
+    }
+}(this, function (DataFactory, HeatMap, WordCloud, Table) {
+    return {
+        HeatMap: {
+            simple: function (callback) {
+                require(["test/DataFactory", "src/other/HeatMap"], function (DataFactory, HeatMap) {
+                    callback(new HeatMap()
+                        .columns(DataFactory.HeatMap.simple.columns)
+                        .data(DataFactory.HeatMap.simple.data)
+                    );
+                });
+            },
+            skew: function (callback) {
+                require(["test/DataFactory", "src/other/HeatMap"], function (DataFactory, HeatMap) {
+                    callback(new HeatMap()
+                        .columns(DataFactory.HeatMap.skew.columns)
+                        .data(DataFactory.HeatMap.skew.data)
+                        .topLeftX(-10000).topLeftY(-10000)
+                        .bottomRightX(10000).bottomRightY(10000)
+                    );
+                });
+            },
+            rapidInterval: function (callback) {
+                require(["test/DataFactory", "src/other/HeatMap"], function (DataFactory, HeatMap) {
+                    var heat = new HeatMap().columns(DataFactory.HeatMap.center.columns).data(DataFactory.HeatMap.center.data)
+                        .topLeftX(-10000).topLeftY(-10000)
+                        .bottomRightX(10000).bottomRightY(10000)
+                        .radius(13).blur(5)
+                    ;
+                    var step = 400;
+                    var renderTimeLimit = 2000;
+                    var mitosisLoop = setInterval(function(){
+                        var prevRenderTime = new Date().getTime();
+                        var newData = [];
+                        heat.data().forEach(function(n){
+                            var splitChance = 1/(heat.data().length/20);
+                            var rand = Math.random();
+                            if(rand <= splitChance * n[2] * 5){
+                                _split(n);
+                            } else if (rand <= splitChance * 100) {
+                                _move(n);
+                            } else {
+                                n[2] = rand >= 0.999999 ? 1 : n[2];
+                                newData.push(n);
+                            }
+                        });
+                        heat.data(newData).render();
+                        if(new Date().getTime() - prevRenderTime > renderTimeLimit){
+                            clearInterval(mitosisLoop);
+                        }
+                        function _split(c){
+                            var o = step + (Math.random()*step);
+                            newData.push(c);
+                            newData.push([[c[0],c[1]-o,c[2]],[c[0]+o,c[1],c[2]],[c[0],c[1]+o,c[2]],[c[0]-o,c[1],c[2]]][Math.floor(Math.random()*4)]);
+                        }
+                        function _move(cell){
+                            var movedCell = [cell[0] + (step*Math.random()*[-1,1][Math.floor(Math.random()*2)]),cell[1] + (step*Math.random()*[-1,1][Math.floor(Math.random()*2)]),cell[2] * 0.99];
+                            if(movedCell[2] > 0.01){
+                                newData.push(movedCell);
+                            }
+                        }
+                    },100);
+                    callback(heat);
+                });
+            }
+        },
+        Image: {
+            layered:function (callback) {
+                require(["test/DataFactory", "src/layout/Layered", "src/layout/AbsoluteSurface", "src/other/Image", "src/other/HeatMap"], function (DataFactory, Layered, AbsoluteSurface, Image, HeatMap) {
+                    var retVal = new Layered()
+                        .addLayer(new AbsoluteSurface().widgetX(0).widgetY(0).widgetWidth(100).widgetHeight(100).widget(
+                                new Image().source(DataFactory.Image.floorplan)
+                            )
+                        )
+                        .addLayer(new AbsoluteSurface().widgetX(0).widgetY(0).widgetWidth(100).widgetHeight(100).opacity(0.66).widget(
+                                new HeatMap()
+                                    .columns(DataFactory.HeatMap.floorplan.columns)
+                                    .data(DataFactory.HeatMap.floorplan.data)
+                            )
+                        )
+                    ;
+                    callback(retVal);
+                });
+            },
+            simple: function (callback) {
+                require(["test/DataFactory", "src/other/Image"], function (DataFactory, Image) {
+                    callback(new Image().source(DataFactory.Image.floorplan));
+                });
+            },
+        },
+        WordCloud: {
+            simple: function (callback) {
+                require(["test/DataFactory", "src/other/WordCloud"], function (DataFactory, WordCloud) {
+                    var words = DataFactory.WordCloud.simple.words.map(function (d) {
+                        return [d, 1 + Math.random() * 100];
+                    });
+                    callback(new WordCloud()
+                        .columns(DataFactory.WordCloud.simple.columns)
+                        .data(words)
+                    );
+                });
+            }
+        },
+        Table: {
+            simple: function (callback) {
+                require(["test/DataFactory", "src/other/Table"], function (DataFactory, Table) {
+                    var table = new Table()
+                        .columns(["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "caaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"])
+                        .data([["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]])
+                    ;
+                    callback(table);
+                    setTimeout(function () {
+                        table
+                            .columns(DataFactory.Table.simple.columns)
+                            .data(DataFactory.Table.simple.data)
+                            .render()
+                        ;
+                    }, 1000);
+                });
+            },
+            widget: function (callback) {
+                require(["test/DataFactory", "src/other/Table"], function (DataFactory, Table) {
+                    callback(new Table()
+                        .columns(DataFactory.Table.widget.columns)
+                        .data(DataFactory.Table.widget.data)
+                        .minWidgetHeight(200)
+                        .minWidgetWidth(200)
+                    );
+                });
+            },
+            large: function (callback) {
+                require(["test/DataFactory", "src/other/Table"], function (DataFactory, Table) {
+                    callback(new Table()
+                    .columns(DataFactory.Table.large.columns)
+                    .data(DataFactory.Table.large.data)
+                    .fixedHeader(true)
+                    );
+                });
+            },
+            totalled: function (callback) {
+                require(["test/DataFactory", "src/other/Table"], function (DataFactory, Table) {
+                    var table = new Table()
+                        .columns(DataFactory.Table.large.columns)
+                        .data(DataFactory.Table.large.data)
+                        .totalledColumns([1, 2, 5, 6, 7])
+                        .totalledLabel("Total")
+                    ;
+                    callback(table);
+                });
+            },
+            formatted: function (callback) {
+                require(["test/DataFactory", "src/other/Table"], function (DataFactory, Table) {
+                    var table = new Table()
+                        .columns(DataFactory.Table.formatted.columns)
+                        .data(DataFactory.Table.formatted.data)
+                    ;
+                    table.fields()[0].format(".2f");
+                    table.fields()[2].format(".6r");
+                    table.fields()[4].type("time").mask("%Y-%_m-%_d").format("%d %b '%y");
+                    callback(table);
+                });
+            }
+        },
+        Toolbar: {
+            simple: function (callback) {
+                require(["test/DataFactory", "src/composite/MegaChart", "src/form/Select"], function (DataFactory, MegaChart, Select) {
+                    callback(new MegaChart()
+                        .chartType("LINE")
+                        .title("Simple Toolbar")
+                        .columns(DataFactory.ND.subjects.columns)
+                        .data(DataFactory.ND.subjects.data));
+                });
+            },
+            megaChart: function (callback) {
+                require(["test/DataFactory", "src/layout/Border", "src/chart/Pie", "src/chart/MultiChartSurface", "src/chart/Line", "src/chart/Column", "src/chart/Step"], function (DataFactory, Border, Pie, MultiChartSurface, Line, Column, Step) {
+                    callback(new Border()
+                        .setContent("top", new Pie()
+                            .columns(DataFactory.ND.subjects.columns)
+                            .data(DataFactory.ND.subjects.data)
+                        )
+                        .setContent("right", new Line()
+                            .columns(DataFactory.ND.subjects.columns)
+                            .data(DataFactory.ND.subjects.data)
+                        )
+                        .setContent("bottom", new Column()
+                            .columns(DataFactory.ND.subjects.columns)
+                            .data(DataFactory.ND.subjects.data)
+                        )
+                        .setContent("left", new Step()
+                            .columns(DataFactory.ND.subjects.columns)
+                            .data(DataFactory.ND.subjects.data)
+                        )
+                        .setContent("center", new MultiChartSurface()
+                            .columns(DataFactory.ND.subjects.columns)
+                            .data(DataFactory.ND.subjects.data)
+                        )
+                    );
+                });
+            },
+        },
+        Legend: {
+            simple: function (callback) {
+                require(["test/DataFactory", "src/layout/Border", "src/chart/Line", "src/other/Legend"], function (DataFactory, Border, Line, Legend) {
+                    var line = new Line()
+                        .columns(DataFactory.ND.ampolar.columns)
+                        .data(DataFactory.ND.ampolar.data)
+                    var legend = new Legend().targetWidget(line);
+                    callback(new Border()
+                        .setContent("center", line)
+                        .setContent("right", legend)
+                    );
+                });
+            }
+        },
+        Html: {
+            simple: function (callback) {
+                require(["src/other/Html"], function (Html) {
+                    var w = new Html();
+                    w.html('<div style="border:1px solid red;padding:10px;margin:20px;font-size:24px;">Text in a div!</div>');
+                    callback(w);
+                });
+            },
+            array: function (callback) {
+                require(["src/other/Html"], function (Html) {
+                    var w = new Html();
+                    var arr = [];
+                    var testCellCount = 300;
+                    for(var i = 0;i<testCellCount;i++){
+                        arr.push('<div style="border:1px solid red;float:left;margin:10px;padding:10px;font-size:20px;">Test '+i+'</div>');
+                    }
+                    w.data(arr);
+                    callback(w);
+                });
+            }
+        }
+    };
+}));
