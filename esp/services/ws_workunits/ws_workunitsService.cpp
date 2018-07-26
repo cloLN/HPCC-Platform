8e0f982806f7bec5b3cabc57988a46983b691318
Russ Whitehead <william.whitehead@lexisnexis.com>
2018-07-26 14:37:21 -0400

    Hello world
    Signed-off-by: Russ Whitehead <william.whitehead@lexisnexis.com>

--- esp/services/ws_workunits/ws_workunitsService.cpp
+++ esp/services/ws_workunits/ws_workunitsService.cpp
@@ -816,7 +816,7 @@ bool CWsWorkunitsEx::onWUResubmit(IEspContext &context, IEspWUResubmitRequest &r
                     queryExtendedWU(wu)->copyWorkUnit(src, false, false);
 
                     SCMStringBuffer token;
-                    wu->setSecurityToken(createToken(wuid.str(), context.queryUserId(), context.queryPassword(), token).str());
+                    wu->setSecurityToken(createToken(wuid.str(), context.queryUserId(), nullptr, token).str());
                 }
 
                 wuids.append(wuid.str());
@@ -946,7 +946,7 @@ bool CWsWorkunitsEx::onWUSchedule(IEspContext &context, IEspWUScheduleRequest &r
             wu->setDebugValueInt("maxRunTime", req.getMaxRunTime(), true);
 
         SCMStringBuffer token;
-        wu->setSecurityToken(createToken(wuid.str(), context.queryUserId(), context.queryPassword(), token).str());
+        wu->setSecurityToken(createToken(wuid.str(), context.queryUserId(), nullptr, token).str());
 
         AuditSystemAccess(context.queryUserId(), true, "Scheduled %s", wuid.str());
     }
